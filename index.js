ad65340f1b9c8078c54c6f818c726562cbcbfefd
Elvasfar <brondby_1964@hotmail.com>
2023-04-12 09:30:39 +0200

    Højde på første kolonne sat i js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 "use strict";
+window.addEventListener("load", start);
+
+function start() {
+  console.log("Chartapp kører");
+
+  const bars = document.querySelectorAll(".bar");
+  const firstBar = bars[0];
+
+  let h = 40;
+  firstBar.style.height = `${h}px`;
+}
